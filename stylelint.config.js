ed35f2c1223b9cb222a190323baed4be5e2d5b93
Amon Sawamura <amon.sawamura@gmail.com>
2020-07-10 21:32:03 +0900

    chore(stylelint): use stylelint-prettier

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,12 @@
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
-  // add your custom config here
-  // https://stylelint.io/user-guide/configuration
-  rules: {},
+  plugins: ['stylelint-scss'],
+  extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['function', 'if', 'for', 'each', 'include', 'mixin', 'content'],
+      },
+    ],
+  },
 };
