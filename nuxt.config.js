67c6baed4a6b99c59c876cabab3194dc16d7ca6a
Amon Sawamura <amon.sawamura@gmail.com>
2020-07-10 22:34:01 +0900

    feat(font): add webfont

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,13 @@ export default {
         content: process.env.npm_package_description || '',
       },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Sawarabi+Gothic&display=swap',
+      },
+    ],
   },
   css: ['./src/assets/scss/global.scss'],
   plugins: [],
